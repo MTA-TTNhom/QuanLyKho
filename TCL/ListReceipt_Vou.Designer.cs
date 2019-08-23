9d5fa550a31b1c6202c571b12b8a9a3b0c7ae4a3
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-08-23 20:32:56 +0700

    update giaodien

---
+++ TCL/ListReceipt_Vou.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace QLKVCGT.GUI
+{
+    partial class ListReceipt_Vou
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.dtpkDayOfReceipt_Vou = new System.Windows.Forms.DateTimePicker();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.dgvListBill = new DevExpress.XtraGrid.GridControl();
+            this.dgvBillView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTotalPrice = new System.Windows.Forms.TextBox();
+            this.tbNgGiao = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.tbDateOfBill = new System.Windows.Forms.TextBox();
+            this.tbCustomerID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbCustomerName = new System.Windows.Forms.TextBox();
+            this.tbNumOfBill = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbEmployeesID = new System.Windows.Forms.TextBox();
+            this.tbEmployeesName = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dgvBillDetail = new DevExpress.XtraGrid.GridControl();
+            this.dgvBillDetailView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListBill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBillView)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBillDetail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBillDetailView)).BeginInit();
+            this.SuspendLayout();
+        }
+
+        #endregion
+    }
+}
