d21e2bc4401deb4395bf4d71ede04391e98cfa6e
Hoangthuy125 <thuy94.mta@gmail.com>
2019-08-24 08:42:35 +0700

    thuy

---
+++ TCL/Receipt_Vou.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TCL
+{
+    partial class Receipt_Vou
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Receipt_Vou));
+            this.nudNumOfItem = new System.Windows.Forms.NumericUpDown();
+            this.btnSaveItem = new DevExpress.XtraEditors.SimpleButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCancelItem = new DevExpress.XtraEditors.SimpleButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnDeleteItem = new DevExpress.XtraEditors.SimpleButton();
+            this.tbItemID = new System.Windows.Forms.TextBox();
+            this.btnEditItem = new DevExpress.XtraEditors.SimpleButton();
+            this.btnAddItem = new DevExpress.XtraEditors.SimpleButton();
+            this.tbItemName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSupplierName = new System.Windows.Forms.TextBox();
+            this.tbSupplierID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTotalPriceItem = new System.Windows.Forms.TextBox();
+            this.btnAddSupplier = new DevExpress.XtraEditors.SimpleButton();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbTotalPriceBill = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.gctBill = new DevExpress.XtraGrid.GridControl();
+            this.dgvBill = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.ItemName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Price = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.NumOfItem = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.TotalPriceOfItem = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.itemId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.btnCancelSupplier = new DevExpress.XtraEditors.SimpleButton();
+            this.tbPriceOnceItem = new System.Windows.Forms.TextBox();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+        }
+
+        #endregion
+    }
+}
