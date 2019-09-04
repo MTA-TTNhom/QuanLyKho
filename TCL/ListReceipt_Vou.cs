405e677f28dae86d020698a234c925bbc673d357
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-04 20:50:17 +0700

    update code xu ly

--- TCL/ListReceipt_Vou.cs
+++ TCL/ListReceipt_Vou.cs
@@ -48,5 +48,36 @@ namespace TCL
             }
             catch { }
         }
+
+        private void binding()
+        {
+            try
+            {
+                tbCustomerID.DataBindings.Clear();
+                tbCustomerID.DataBindings.Add("Text", dgvListBill.DataSource, "Mã nhà cung cấp");
+                tbCustomerName.DataBindings.Clear();
+                tbCustomerName.DataBindings.Add("Text", dgvListBill.DataSource, "Tên nhà cung cấp");
+                tbEmployeesID.DataBindings.Clear();
+                tbEmployeesID.DataBindings.Add("Text", dgvListBill.DataSource, "Mã nhân viên");
+                tbEmployeesID.DataBindings.Clear();
+                tbEmployeesName.DataBindings.Add("Text", dgvListBill.DataSource, "Tên nhân viên");
+                tbNumOfBill.DataBindings.Clear();
+                tbNumOfBill.DataBindings.Add("Text", dgvListBill.DataSource, "số phiếu nhập");
+                tbDateOfBill.DataBindings.Clear();
+                tbDateOfBill.DataBindings.Add("Text", dgvListBill.DataSource, "Ngày nhập");
+                tbTotalPrice.DataBindings.Clear();
+                tbTotalPrice.DataBindings.Add("Text", dgvListBill.DataSource, "Tổng tiền");
+                tbNumOfBill.DataBindings.Clear();
+                tbNgGiao.DataBindings.Add("Text", dgvListBill.DataSource, "Người giao");
+
+            }
+            catch { }
+        }
+
+        private void dgvBillView_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            int numOfBill = int.Parse(dgvBillView.GetFocusedRowCellValue("Số phiếu nhập").ToString());
+            dgvBillDetail.DataSource = ReceiptVouControl.Instance.DataSource_GetDetailBillByID(numOfBill);
+        }
     }
 }
