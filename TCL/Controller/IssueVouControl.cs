52310f0ca4ae030e4888f7343f0307d985367483
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-05 14:37:25 +0700

    update issueVoucontrol

--- TCL/Controller/IssueVouControl.cs
+++ TCL/Controller/IssueVouControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace QLKVCGT.Controller
 {
     class IssueVouControl
     {
+        private static IssueVouControl instance;
+
+        public static IssueVouControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new IssueVouControl();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
     }
 }
