6534e53cdfefafd9ea831af90a7aade2434341c9
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-08 15:54:35 +0700

    Update ItemControl.cs

--- TCL/Controller/ItemControl.cs
+++ TCL/Controller/ItemControl.cs
@@ -5,9 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TCL.Controller
+namespace QLKVCGT.Controller
 {
     class ItemControl
     {
+        private static ItemControl instance;
+
+        public static ItemControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ItemControl();
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+        private ItemControl() { }
+
+        public DataTable DataSource_GetItem()
+        {
+            string query = @"exec  SP_getItem ";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { });
+        }
     }
 }
