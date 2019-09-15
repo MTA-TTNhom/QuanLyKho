2f4e490aa63dba7a2b65d5d725dd40fd6bda082d
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-15 17:16:20 +0700

    Update LoginControl.cs

--- TCL/Controller/LoginControl.cs
+++ TCL/Controller/LoginControl.cs
@@ -9,5 +9,32 @@ namespace QLKVCGT.Controller
 {
     class LoginControl
     {
+        private static LoginControl instance;
+
+        public static LoginControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LoginControl();
+                }
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+        private LoginControl()
+        {
+        }
+
+        public DataTable DataSource_Login(string _userName, string _passWork)
+        {
+            string query = @"exec  SP_GETINFOBYUSERNAME @userName, @pass";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { _userName, _passWork });
+        }
     }
 }
