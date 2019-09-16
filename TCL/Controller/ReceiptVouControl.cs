a4bffb92ad1b9605679f7b7a51a5ad7a3f8ad232
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-16 14:23:38 +0700

    Update ReceiptVouControl.cs

--- TCL/Controller/ReceiptVouControl.cs
+++ TCL/Controller/ReceiptVouControl.cs
@@ -8,5 +8,39 @@ namespace TCL.Controller
 {
     class ReceiptVouControl
     {
+        public static ReceiptVouControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ReceiptVouControl();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+        private ReceiptVouControl() { }
+
+
+        public DataTable DataSource_GetListBill()
+        {
+            string query = @"exec SP_getListBillIn ";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { });
+        }
+        public int InsertBill(int _employessId, int _supplierID, int _totalPrice)
+        {
+            string query = @"exec SP_insertBillIn @employeesId, @supplierID, @totalPrice";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _employessId, _supplierID, _totalPrice });
+        }
+
+
+        public int InsertBillDetail(int _itemId, int _billId, int _numOfItem)
+        {
+            string query = @"exec SP_insertBillInDetail @itemId, @billId, @numOfItem";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _itemId, _billId, _numOfItem });
+        }
     }
 }
