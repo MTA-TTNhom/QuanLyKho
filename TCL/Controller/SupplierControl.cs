f97047da5f4250435f217d3c73912099beae820a
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-18 16:26:05 +0700

    Update SupplierControl.cs

--- TCL/Controller/SupplierControl.cs
+++ TCL/Controller/SupplierControl.cs
@@ -34,5 +34,30 @@ namespace QLKVCGT.Controller
             string query = @"exec SP_getSupplier";
             return DataProvider.Instances.ExecuteQuery(query, new object[] { });
         }
+        public DataTable DataSource_getSupplierByIdSup(int id)
+        {
+            string query = @"exec SP_getSupplierByIdSup @id";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { id });
+        }
+        public DataTable DataSource_GetSupplierByName(string name)
+        {
+            string query = @"exec SP_getSupplierByName @name";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { name });
+        }
+        public int InsertSupplier(string _Supplier, string _Address, string _Phone, string _Fax, string _Email, string _Tax_Ident_Num)
+        {
+            string query = @"exec SP_InsertSupplier @SupplierName, @Address, @Phone, @Fax, @Email, @Tax_Ident_Num";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _Supplier, _Address, _Phone, _Fax, _Email, _Tax_Ident_Num });
+        }
+        public int UpdateSupplier(string _Supplier, string _Address, string _Phone, string _Fax, string _Email, string _Tax_Ident_Num)
+        {
+            string query = @"exec SP_UpdateSupplier @SupplierName, @Address, @Phone, @Fax, @Email, @Tax_Ident_Num";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _Supplier, _Address, _Phone, _Fax, _Email, _Tax_Ident_Num });
+        }
+        public int DeleteSupplier(string _Supplier)
+        {
+            string query = @"exec SP_DeleteSupplier @SupplierName ";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _Supplier });
+        }
     }
 }
