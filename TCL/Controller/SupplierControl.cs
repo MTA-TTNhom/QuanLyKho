48633a1c3e8761d8c637251a452e58bc723aaf9c
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-18 15:25:46 +0700

    Update SupplierControl.cs

--- TCL/Controller/SupplierControl.cs
+++ TCL/Controller/SupplierControl.cs
@@ -9,5 +9,30 @@ namespace QLKVCGT.Controller
 {
     class SupplierControl
     {
+        private static SupplierControl instance;
+
+        public static SupplierControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SupplierControl();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+        private SupplierControl()
+        {
+        }
+
+        public DataTable DataSource_getSupplier()
+        {
+            string query = @"exec SP_getSupplier";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { });
+        }
     }
 }
