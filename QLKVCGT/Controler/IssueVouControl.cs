7e0c3885b5bcf08a01f8bf0a85649930e5e07984
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-25 00:30:36 +0700

    Huynh

--- QLKVCGT/Controler/IssueVouControl.cs
+++ QLKVCGT/Controler/IssueVouControl.cs
@@ -11,22 +11,7 @@ namespace QLKVCGT.Controler
     {
         private static IssueVouControl instance;
 
-        public static IssueVouControl Instance
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new IssueVouControl();
-                return instance;
-            }
-
-            private set
-            {
-                instance = value;
-            }
-        }
-        private IssueVouControl() { }
-
+        
        
         public DataTable DataSource_GetListBill()
         {
