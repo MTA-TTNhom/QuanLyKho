b9348e70681fcdd630ad0f35b3490f61d6de3313
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-25 08:55:09 +0700

    Update Item_Store.cs

--- TCL/GUI/Item_Store.cs
+++ TCL/GUI/Item_Store.cs
@@ -16,5 +16,35 @@ namespace TCL
         {
             InitializeComponent();
         }
+		        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtMatKhau.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtMatKhau.UseSystemPasswordChar = true;
+                if (e.KeyCode == Keys.Enter)
+                    btnDangNhap.Focus();
+            }
+        }
+        private void txtTaiKhoan_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtMatKhau.Focus();
+        }
+        private void txtMatKhau_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnDangNhap.Focus();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnDangNhap.Focus();
+		}
     }
 }
