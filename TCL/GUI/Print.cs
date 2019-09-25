4d142e833383e58c8bf48e1ee6956863e6d04733
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-25 08:57:24 +0700

    update

--- TCL/GUI/Print.cs
+++ TCL/GUI/Print.cs
@@ -16,5 +16,34 @@ namespace TCL
         {
             InitializeComponent();
         }
+		
+        private int ChangeInfo()
+        {
+            int _id = Convert.ToInt32(tbID.Text.Trim(' '));
+            DateTime _dateOfBirth = dtpkDateOfBirth.Value;
+            string _name = tbName.Text.Trim(' ');
+            string _userName = tbUserName.Text.Trim(' ');
+            int _sex;
+            if (cbbSex.Text == "Nam") _sex = 1;
+            else _sex = 0;
+            float _salary = (float)Convert.ToDouble(tbSalary.Text);
+            string _telephoneNumber = tbTelephoneNumber.Text.Trim(' ');
+            string _country = tbCountry.Text.Trim(' ');
+            return AcountInfoControl.Instance.ChangeAcount(_id, _name, _dateOfBirth, _sex, _userName, _country, _telephoneNumber);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ChangeInfo() == 1)
+            {
+                MessageBox.Show("Thành công!");
+            }
+            load();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            load();
+        }
     }
 }
