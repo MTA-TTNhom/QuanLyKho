b2abaea401ce030b0bdf4b7aad569612ebeffc32
asdcxsd <nguyenmanhthien98@gmail.com>
2019-09-25 08:55:44 +0700

    Update Item_Store.cs

--- TCL/GUI/Item_Store.cs
+++ TCL/GUI/Item_Store.cs
@@ -46,5 +46,40 @@ namespace TCL
             if (e.KeyCode == Keys.Enter)
                 btnDangNhap.Focus();
 		}
+		        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string _oldPass = tbOldPass.Text.Trim(' ');
+            string newPass = tbNewPass.Text.Trim(' ');
+            string rePass = tbRePass.Text.Trim(' ');
+            if (rePass != newPass || rePass == "" || newPass == "")
+            {
+                MessageBox.Show("Mật khẩu mới không trùng nhau hoặc không hợp lệ!");
+                return;
+            }
+            if (_oldPass != oldPass)
+            {
+                MessageBox.Show("Mật khẩu cũ không đúng!");
+                return;
+            }
+
+            Controler.ChangePassControl change = new Controler.ChangePassControl();
+            int i = change.ChangePass(id, newPass);
+            if (i > 0)
+            {
+                MessageBox.Show("Thành công!");
+                this.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+		
+		        public void init(int _id, string _oldPass)
+        {
+            id = _id;
+            oldPass = _oldPass;
+        }
     }
 }
