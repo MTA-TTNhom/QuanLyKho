2b9c51df33ffeb015f70faad9428ec78b860fc2b
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-27 00:20:45 +0700

    Huynh

--- QLKVCGT/Controler/CustomerControl.cs
+++ QLKVCGT/Controler/CustomerControl.cs
@@ -13,11 +13,7 @@ namespace QLKVCGT.Controler
 
         
         
-        public DataTable DataSource_getCustomer()
-        {
-            string query = @"exec SP_getCustomer";
-            return DataProvider.Instances.ExecuteQuery(query, new object[] { });
-        }
+        
         public DataTable DataSource_getCustomerByIdCus(int id)
         {
             string query = @"exec SP_getCustomerByIdCus @id";
