642b7a548d75d4e7bf0ae98e231df89423462d24
Khoabdattt52 <phambangbang1235@gmail.com>
2019-09-27 08:30:56 +0700

    Update Frm1.cs

--- TCL/Form1.cs
+++ TCL/Form1.cs
@@ -16,5 +16,76 @@ namespace TCL.GUI
         {
             InitializeComponent();
         }
+        private int typeOfEmployees;
+        private int idEmployees;
+        private string passWord;
+        private string nameEmployees;
+
+        public int TypeOfEmployees
+        {
+            get
+            {
+                return typeOfEmployees;
+            }
+
+            set
+            {
+                typeOfEmployees = value;
+            }
+        }
+
+        public int IdEmployees
+        {
+            get
+            {
+                return idEmployees;
+            }
+
+            set
+            {
+                idEmployees = value;
+            }
+        }
+
+        public string PassWord
+        {
+            get
+            {
+                return passWord;
+            }
+
+            set
+            {
+                passWord = value;
+            }
+        }
+
+        public string NameEmployees
+        {
+            get
+            {
+                return nameEmployees;
+            }
+
+            set
+            {
+                nameEmployees = value;
+            }
+        }
+
+        public void init(int _typeOfEmployees, int _idEmployees, string _passWord, string _name)
+        {
+            TypeOfEmployees = _typeOfEmployees;
+            IdEmployees = _idEmployees;
+            passWord = _passWord;
+            nameEmployees = _name;
+        }
+        private void VisibleRpg(bool e)
+        {
+            rpgEmployees.Visible = e;
+            rpgStock.Visible = e;
+
+        }
+
     }
 }
